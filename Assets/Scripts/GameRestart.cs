13a96caffbe4fbf76c9372ca744112fd79fac292
Albert Mukiria <albert.mukiria@gmail.com>
2020-12-17 16:40:14 +0300

    First commit
    Added Unity Projects to GitHub

---
+++ Assets/Scripts/GameRestart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRestart : MonoBehaviour
+{
+    // respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the NextLevel function in the game manager
+			GameManager.gm.restartFromBeginning();
+		}
+	}
+    
+}
